62b453f4f99e4eb8a7fbccaf60d49a73544e5d5e
mhsmans <mh.smans@gmail.com>
2019-05-11 17:27:01 +0200

    Brute force added.

--- src/calculate/number/Main.java
+++ src/calculate/number/Main.java
@@ -1,6 +1,6 @@
 package calculate.number;
 
-import calculate.number.uninformedSearch.DepthFirstSearch;
+import calculate.number.uninformedSearch.BruteForce;
 
 import java.util.Stack;
 
@@ -9,22 +9,22 @@ public class Main {
     public static void main(String[] args) {
 
         BinaryTree tree = new BinaryTree();
-        tree.root = new Node(6);
-        tree.root.leftChild = new Node(10);
-        tree.root.rightChild = new Node(25);
-        tree.root.leftChild.leftChild = new Node(75);
-        tree.root.leftChild.rightChild = new Node(5);
-        tree.root.rightChild.leftChild = new Node(50);
+        tree.root = new Node(25);
+        tree.root.leftChild = new Node(50);
+        tree.root.rightChild = new Node(7);
+        tree.root.leftChild.leftChild = new Node(3);
+        tree.root.leftChild.rightChild = new Node(4);
+        tree.root.rightChild.leftChild = new Node(9);
 
         Stack<Integer> stack = tree.createPreOrderStack();
-        int goalNumber = 728;
-        Stack<Integer> st = new Stack<>();
-        st.push(1);
-        st.push(2);
-        st.push(4);
+        int goalNumber = 330;
 
-        DepthFirstSearch dfs = new DepthFirstSearch(goalNumber, st);
-        dfs.calculate();
+        //DepthFirstSearch dfs = new DepthFirstSearch(goalNumber, st);
+        //dfs.calculate();
+
+        BruteForce bruteForce = new BruteForce(goalNumber, stack);
+        // Given path is incorrect. This is caused by switching x and y values when x < y.
+        bruteForce.calculate();
     }
 
 }
---
+++ src/calculate/number/uninformedSearch/BruteForce.java
@@ -0,0 +1,85 @@
+package calculate.number.uninformedSearch;
+
+import java.util.Stack;
+
+public class BruteForce {
+
+    private int goalNumber;
+    private Stack<Integer> gameStack;
+
+    public BruteForce(int goalNumber, Stack<Integer> stack) {
+        this.goalNumber = goalNumber;
+        this.gameStack = stack;
+    }
+
+    private void calculate(Stack<Integer> stack, int currentValue, String calculationPath) {
+        if (!stack.empty()) {
+            int topNumber = stack.pop();
+            operate(currentValue, topNumber, stack, calculationPath);
+        }
+    }
+
+    private void operate(int x, int y, Stack<Integer> stack, String calculationPath) {
+        // switch incoming integers if x < y
+        if (x < y){
+            int t = x;
+            x = y;
+            y = t;
+        }
+
+        // Deep copies of stack
+        Stack<Integer> addStack = (Stack<Integer>)stack.clone();
+        Stack<Integer> multiplyStack = (Stack<Integer>)stack.clone();
+        Stack<Integer> subtractStack = (Stack<Integer>)stack.clone();
+        Stack<Integer> divideStack = (Stack<Integer>)stack.clone();
+
+        // Paths
+        String addPath = calculationPath + " + " + y;
+        System.out.print(addPath);
+        System.out.print(" = " + (x + y) + "\n");
+
+        if (x + y == goalNumber) {
+            System.out.println("FOUND!!!");
+        }
+
+        String multiplyPath = calculationPath + " * " + y;
+        System.out.print(multiplyPath);
+        System.out.print(" = " + (x * y) + "\n");
+
+        if (x * y == goalNumber) {
+            System.out.println("FOUND!!!");
+        }
+
+        String subtractPath = calculationPath + " - " + y;
+        System.out.print(subtractPath);
+        System.out.print(" = " + (x - y) + "\n");
+
+        if (x - y == goalNumber) {
+            System.out.println("FOUND!!!");
+        }
+
+        // divide by zero is impossible.
+        if (y != 0) {
+            String dividePath = calculationPath + " / " + y;
+            System.out.print(dividePath);
+            System.out.print(" = " + (x / y) + "\n");
+
+            if (x / y == goalNumber) {
+                System.out.println("FOUND!!!");
+            }
+            calculate(divideStack, x / y, dividePath);
+        }
+
+        // Calculation method calls
+        calculate(addStack, x + y, addPath);
+        calculate(multiplyStack, x * y, multiplyPath);
+        calculate(subtractStack, x - y, subtractPath);
+    }
+
+    public void calculate() {
+        int startValue = this.gameStack.pop();
+        String startPath = "path: ";
+        startPath += startValue;
+        calculate(this.gameStack, startValue, startPath);
+    }
+}
